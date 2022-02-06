28e6146de9f25fa0af7673be28c5bdfd0b782364
Samay Pusarla <samaypusarla1@gmail.com>
2022-02-05 20:26:06 -0800

    Did many things, bullet 5 (#3)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict"
+var jwttoken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,6 +9,8 @@ const app = express()
 app.listen(3000)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+var cors = require('cors')
+app.use(cors())
 console.log('Node. js Express server is running on port 3000... ')
 
 app.use(function (req, res, next) {
@@ -21,7 +24,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
@@ -34,13 +37,36 @@ app.use(function (req, res, next) {
 app.get('/v1/weather', get_weather)
 
 function get_weather(request, response) {
-    response.json({ "coord": { "lon": -123.262, "lat": 44.5646 }, "weather": [{ "id": 801, "main": "Clouds", "description": "few clouds", "icon": "02n" }], "base": "stations", "main": { "temp": 58, "feels_like": 49.98, "temp_min": 48.09, "temp_max": 55.45, "pressure": 1023, "humidity": 78 }, "visibility": 10000, "wind": { "speed": 0, "deg": 0 }, "clouds": { "all": 20 }, "dt": 1641958461, "sys": { "type": 2, "id": 2040223, "country": "US", "sunrise": 1641916079, "sunset": 1641948820 }, "timezone": -28800, "id": 5720727, "name": "Corvallis", "cod": 200 })
+    const authHeader = request.headers['authorization']
+
+    console.log(request.header['authorization'])
+    if (jwttoken == null) return response.sendStatus(401)
+
+    if (authHeader == jwttoken) {
+        response.json({
+            "coord": { "lon": -123.262, "lat": 44.5646 }, "weather": [{ "id": 801, "main": "Clouds", "description": "few clouds", "icon": "02n" }], "base": "stations", "main": { "temp": 58, "feels_like": 49.98, "temp_min": 48.09, "temp_max": 55.45, "pressure": 1023, "humidity": 78 }, "visibility": 10000, "wind": { "speed": 0, "deg": 0 }, "clouds": { "all": 20 }, "dt": 1641958461, "sys": { "type": 2, "id": 2040223, "country": "US", "sunrise": 1641916079, "sunset": 1641948820 }, "timezone": -28800, "id": 5720727, "name": "Corvallis", "cod": 200
+        });
+    } else {
+        return response.sendStatus(403)
+    }
 }
 
 app.get('/v1/hello', get_hello)
 
+
 function get_hello(request, response) {
-    response.json({"greeting" : "Hello Samay"})
+    const authHeader = request.headers['authorization']
+    if (jwttoken == null) return response.sendStatus(401)
+
+    if (authHeader == jwttoken) {
+        response.json({
+            "greeting": "Hello Samay"
+        })
+    } else {
+        return response.sendStatus(403)
+    }
+
+
 }
 
 app.post('/v1/auth', post_auth)
@@ -51,7 +77,7 @@ function post_auth(request, response) {
     var password = request.body.password;
 
     response.json({
-        "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+        "access_token": jwttoken,
         "expires": "2012-04-23T18:25:43.511Z"
-      })
+    })
 }
